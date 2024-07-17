87781e1aa29fa96907243f75094b23b221f34be6
Thorsten Grassmann <Thorsten.Grassmann@zae-bayern.de>
2024-07-17 10:45:32 +0200

    Wiss korrekte Bezeichnungen, Infobuttons zugefügt.

--- datenerfassung.js
+++ datenerfassung.js
@@ -105,3 +105,196 @@ document.addEventListener('DOMContentLoaded', function() {
     })(box);
   }
 })();
+
+// Function to update form fields based on selection
+    function updateFormFields(selectedSiteId = null) {
+        var select = document.getElementById('siteSelect');
+
+        // If a specific site ID is provided, use it
+        if (selectedSiteId !== null) {
+            select.value = selectedSiteId;
+        }
+
+        selectedSiteId = select.value;
+
+        if (selectedSiteId === "new") {
+            // Clear all form fields for new site
+            document.getElementById('company').value = "";
+            document.getElementById('name').value = "";
+            // Clear checkboxes
+            document.querySelectorAll('input[type=checkbox]').forEach(checkbox => checkbox.checked = false);
+            // Add other fields as needed
+        } else {
+            // Find the selected site data
+            var selectedSite = sitesData.find(site => site.id == selectedSiteId);
+            console.log('Selected Site:', selectedSite); // Debugging
+
+            if (selectedSite) {
+                // Populate form fields with selected site data, use empty string as default if undefined
+                document.getElementById('company').value = selectedSite.site_data.company || "";
+                document.getElementById('name').value = selectedSite.site_name || "";
+
+                // Clear checkboxes
+                document.querySelectorAll('input[type=checkbox]').forEach(checkbox => checkbox.checked = false);
+
+                //set type radio select
+                if (selectedSite.site_data.type == "1") {
+                    document.getElementById('type-1').checked = true;
+                }
+                if (selectedSite.site_data.type == "2") {
+                    document.getElementById('type-2').checked = true;
+                }
+                if (selectedSite.site_data.type == "3") {
+                    document.getElementById('type-3').checked = true;
+                }
+
+                //set org checkboxes
+                if (0 in selectedSite.site_data.org) {
+                    document.getElementById('org-0').checked = true;
+                }
+                if (selectedSite.site_data.org.indexOf("servitex") !== -1) {
+                    document.getElementById('org-servitex').checked = true;
+                }
+                if (selectedSite.site_data.org.indexOf("sitex") !== -1) {
+                    document.getElementById('org-sitex').checked = true;
+                }
+                if (selectedSite.site_data.org.indexOf("dressline") !== -1) {
+                    document.getElementById('org-dressline').checked = true;
+                }
+                if (selectedSite.site_data.org.indexOf("nwdzentratex") !== -1) {
+                    document.getElementById('org-nwdzentratex').checked = true;
+                }
+                if (selectedSite.site_data.org.indexOf("lavantex") !== -1) {
+                    document.getElementById('org-lavantex').checked = true;
+                }
+                if (selectedSite.site_data.org.indexOf("tsa") !== -1) {
+                    document.getElementById('org-tsa').checked = true;
+                }
+                if (selectedSite.site_data.org.indexOf("leosystem") !== -1) {
+                    document.getElementById('org-leosystem').checked = true;
+                }
+                if (selectedSite.site_data.org.indexOf("dbl") !== -1) {
+                    document.getElementById('org-dbl').checked = true;
+                }
+                if (selectedSite.site_data.org.indexOf("diemietwaesche.de") !== -1) {
+                    document.getElementById('org-diemietwaesche.de').checked = true;
+                }
+
+                // set work checkboxes
+                if (selectedSite.site_data.work.indexOf("wtabwasserfrisch") !== -1) {
+                    document.getElementById('work1-wtabwasserfrisch').checked = true;
+                    document.getElementById('work3-wtabwasserfrisch').checked = true;
+                }
+                if (selectedSite.site_data.work.indexOf("wrabluftzufrisch") !== -1) {
+                    document.getElementById('work1-wrabluftzufrisch').checked = true;
+                }
+                if (selectedSite.site_data.work.indexOf("wrabluftzuwasser") !== -1) {
+                    document.getElementById('work1-wrabluftzuwasser').checked = true;
+                    document.getElementById('work3-wrabluftzuwasser').checked = true;
+                }
+                if (selectedSite.site_data.work.indexOf("wasserrueck") !== -1) {
+                    document.getElementById('work1-wasserrueck').checked = true;
+                    document.getElementById('work2-wasserrueck').checked = true;
+                    document.getElementById('work3-wasserrueck').checked = true;
+                }
+                if (selectedSite.site_data.work.indexOf("wassermehrfach") !== -1) {
+                    document.getElementById('work1-wassermehrfach').checked = true;
+                }
+                if (selectedSite.site_data.work.indexOf("abwassersiebfilter") !== -1) {
+                    document.getElementById('work1-abwassersiebfilter').checked = true;
+                }
+                if (selectedSite.site_data.work.indexOf("abwasserfaellung") !== -1) {
+                    document.getElementById('work1-abwasserfaellung').checked = true;
+                }
+                if (selectedSite.site_data.work.indexOf("abwassermikrofiltration") !== -1) {
+                    document.getElementById('work1-abwassermikrofiltration').checked = true;
+                }
+                if (selectedSite.site_data.work.indexOf("abwasserneutralis") !== -1) {
+                    document.getElementById('work1-abwasserneutralis').checked = true;
+                }
+                if (selectedSite.site_data.work.indexOf("abwasserbiologie") !== -1) {
+                    document.getElementById('work1-abwasserbiologie').checked = true;
+                }
+                if (selectedSite.site_data.work.indexOf("dampfkessel") !== -1) {
+                    document.getElementById('work1-dampfkessel').checked = true;
+                }
+                if (selectedSite.site_data.work.indexOf("dampfsystem") !== -1) {
+                    document.getElementById('work1-dampfsystem').checked = true;
+                }
+                if (selectedSite.site_data.work.indexOf("kontakt") !== -1) {
+                    document.getElementById('work2-kontakt').checked = true;
+                    document.getElementById('work3-kontakt').checked = true;
+                }
+                if (selectedSite.site_data.work.indexOf("kuehl") !== -1) {
+                    document.getElementById('work2-kuehl').checked = true;
+                    document.getElementById('work3-kuehl').checked = true;
+                }
+                if (selectedSite.site_data.work.indexOf("wrabluftzufluft") !== -1) {
+                    document.getElementById('work3-wrabluftzufluft').checked = true;
+                }
+
+
+                }
+            }
+        }
+    
+
+    window.onload = function() {
+
+            // Extract site_id from the URL, if available
+            var urlParams = new URLSearchParams(window.location.search);
+            var siteIdFromUrl = urlParams.has('site_id') ? urlParams.get('site_id') : null;
+
+            // Populate the dropdown and select the appropriate site if siteIdFromUrl is provided
+            populateDropdown(siteIdFromUrl);
+
+            document.getElementById('siteSelect').addEventListener('change', function() {
+                updateFormFields();
+            });
+
+            document.getElementById('siteSelect').style.visibility = "hidden";
+
+        document.getElementById('deleteSite').addEventListener('click', function() {
+
+        event.preventDefault(); // dont transmit form
+
+        var select = document.getElementById('siteSelect');
+        var selectedSiteId = select.value;
+
+        if (selectedSiteId === "new") {
+            alert("Bitte wählen Sie eine vorhandene Seite aus, um sie zu löschen.");
+            return;
+        }
+
+        if (confirm("Sind Sie sicher, dass Sie diese Seite löschen möchten?")) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", "backend/delete_site.php", true);
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    alert(xhr.responseText);
+                    // Remove the deleted entry from the dropdown and select the previous one
+                    var optionToRemove = select.querySelector('option[value="' + selectedSiteId + '"]');
+                    if (optionToRemove) {
+                        var previousOption = optionToRemove.previousElementSibling;
+                        optionToRemove.remove();
+                        if (previousOption) {
+                            previousOption.selected = true;
+                        } else {
+                            select.value = "new";
+                        }
+                        updateFormFields();
+                    }
+                    // Redirect to dashboard.php after deletion
+                    window.location.href = 'dashboard.php';
+                }
+            };
+            xhr.send("id=" + selectedSiteId);
+        }
+        });
+
+        // If a site ID is provided in the URL, update the form fields
+        if (siteIdFromUrl !== null) {
+            updateFormFields(siteIdFromUrl);
+        }
+    }
--- datenerfassung.php
+++ datenerfassung.php
@@ -195,7 +195,7 @@ function populateDropdown() {
             <div class="input">
                 <div class="opts">
                     <input type="checkbox" class="check" name="work[]" id="work1-wtabwasserfrisch" value="wtabwasserfrisch"><label for="work1-wtabwasserfrisch"></label>
-                    <label for="work1-wtabwasserfrisch">Wärmetauscher Abwasser zu Frischwasser</label><br>
+                    <label for="work1-wtabwasserfrisch">Wärmewandler Abwasser zu Frischwasser</label><br>
                     <input type="checkbox" class="check" name="work[]" id="work1-wrabluftzufrisch" value="wrabluftzufrisch"><label for="work1-wrabluftzufrisch"></label>
                     <label for="work1-wrabluftzufrisch">Wärmerückgewinnung Abluft zu Frischluft</label><br>
                     <input type="checkbox" class="check" name="work[]" id="work1-wrabluftzuwasser" value="wrabluftzuwasser"><label for="work1-wrabluftzuwasser"></label>
@@ -211,11 +211,11 @@ function populateDropdown() {
                     <input type="checkbox" class="check" name="work[]" id="work1-abwassermikrofiltration" value="abwassermikrofiltration"><label for="work1-abwassermikrofiltration"></label>
                     <label for="work1-abwassermikrofiltration">Abwasserbehandlung: Mikro-/Ultrafiltration</label><br>
                     <input type="checkbox" class="check" name="work[]" id="work1-abwasserneutralis" value="abwasserneutralis"><label for="work1-abwasserneutralis"></label>
-                    <label for="work1-abwasserneutralis">Abwasserbehandlung: Neutralis</label><br>
+                    <label for="work1-abwasserneutralis">Abwasserbehandlung: Neutralisation</label><br>
                     <input type="checkbox" class="check" name="work[]" id="work1-abwasserbiologie" value="abwasserbiologie"><label for="work1-abwasserbiologie"></label>
                     <label for="work1-abwasserbiologie">Abwasserbehandlung: Biologie</label><br>
                     <input type="checkbox" class="check" name="work[]" id="work1-dampfkessel" value="dampfkessel"><label for="work1-dampfkessel"></label>
-                    <label for="work1-dampfkessel">Dampfkessel: Abluft-Wärmetausch (ECO)</label><br>
+                    <label for="work1-dampfkessel">Dampfkessel: Abluft-Wärmewandler (ECO)</label><br>
                     <input type="checkbox" class="check" name="work[]" id="work1-dampfsystem" value="dampfsystem"><label for="work1-dampfsystem"></label>
                     <label for="work1-dampfsystem">Dampf-System: Brüdendampf-Nutzung</label><br>
                 </div><br>
@@ -243,7 +243,7 @@ function populateDropdown() {
                     <input type="checkbox" class="check" name="work[]" id="work3-kontakt" value="kontakt"><label for="work3-kontakt"></label>
                     <label for="work3-kontakt">Kontaktwasseraufbereitung</label><br>
                     <input type="checkbox" class="check" name="work[]" id="work3-wtabwasserfrisch" value="wtabwasserfrisch"><label for="work3-wtabwasserfrisch"></label>
-                    <label for="work3-wtabwasserfrisch">Wärmetauscher Abwasser zu Frischwasser</label><br>
+                    <label for="work3-wtabwasserfrisch">Wärmewandler Abwasser zu Frischwasser</label><br>
                     <input type="checkbox" class="check" name="work[]" id="work3-wrabluftzufwasser" value="wrabluftzufwasser"><label for="work3-wrabluftzufwasser"></label>
                     <label for="work3-wrabluftzufwasser">Wärmerückgewinnung Abluft zu Frischwasser</label><br>
                     <input type="checkbox" class="check" name="work[]" id="work3-wrabluftzufluft" value="wrabluftzufluft"><label for="work3-wrabluftzufluft"></label>
@@ -334,34 +334,56 @@ function populateDropdown() {
         </div>
         <p><b>Im gewählten Zeitraum wurden verbraucht:</b></p>
         <div class="input">
-            <label class="left" title="Wasser [m³]">Wasser [m³]:</label>
+            <label class="left" title="Wasser [m³]">Frischwasser [m³]:</label>
             <input type="text" class="text" name="timespans[0][wasser]" id="wasser-1" value="">
+            <span class="info-button">
+                <div class="tooltip">Informationen</div>
+            </span>
             <div class="inner note">*inkl. Kessel und Sozialbereich</div>
         </div>
         <div class="input">
             <label class="left" title="Strom [kWh]">Strom [kWh]:</label>
             <input type="text" class="text" name="timespans[0][strom]" id="strom-1" value="">
+            <span class="info-button">
+                <div class="tooltip">Informationen</div>
+            </span>
             <div class="inner note">*inkl. Selbsterzeugung</div>
         </div>
         <div class="input">
-            <label class="left" title="Öl [l]">Öl [l]:</label>
+            <label class="left" title="Öl [l]">Heizöl [l]:</label>
             <input type="text" class="text" name="timespans[0][oel]" id="oel-1" value="">
+            <span class="info-button">
+                <div class="tooltip">Informationen</div>
+            </span>
         </div>
+        <!-- TODO: Alternative: Öl (Heizwert) [kWh] -->
         <div class="input">
-            <label class="left" title="Gas [kWh]">Gas [kWh]:</label>
+            <label class="left" title="Gas [kWh]">Erdgas (Brennwert) [kWh]:</label>
             <input type="text" class="text" name="timespans[0][gas]" id="gas-1" value="">
+            <span class="info-button">
+                <div class="tooltip">Informationen</div>
+            </span>
         </div>
         <div class="input">
-            <label class="left" title="Holzpellets [kWh]">Holzpellets [kWh]:</label>
+            <label class="left" title="Holzpellets [kWh]">Holzpellets (Heizwert) [kWh]:</label>
             <input type="text" class="text" name="timespans[0][holz]" id="holz-1" value="">
+            <span class="info-button">
+                <div class="tooltip">Informationen</div>
+            </span>
         </div>
         <div class="input">
-            <label class="left" title="sonstige Energieträger [kWh]">sonstige Energieträger [kWh]:</label>
+            <label class="left" title="sonstige Energieträger [kWh]">sonstige Energieträger (Brenn-/Heizwert) [kWh]:</label>
             <input type="text" class="text" name="timespans[0][sonstigeenergie]" id="sonstigeenergie-1" value="">
+            <span class="info-button">
+                <div class="tooltip">Informationen</div>
+            </span>
         </div>
         <div class="input">
             <label class="left" title="Waschmittel [kg]">Waschmittel [kg]:</label>
             <input type="text" class="text" name="timespans[0][waschmittel]" id="waschmittel-1" value="">
+            <span class="info-button">
+                <div class="tooltip">Informationen</div>
+            </span>
             <div class="inner note">*inkl. Waschhilfsmittel</div>
         </div>
     </div>
@@ -385,201 +407,6 @@ function populateDropdown() {
 		</div>
 
 </form>
-
-<script>
-// Function to update form fields based on selection
-    function updateFormFields(selectedSiteId = null) {
-        var select = document.getElementById('siteSelect');
-
-        // If a specific site ID is provided, use it
-        if (selectedSiteId !== null) {
-            select.value = selectedSiteId;
-        }
-
-        selectedSiteId = select.value;
-
-        if (selectedSiteId === "new") {
-            // Clear all form fields for new site
-            document.getElementById('company').value = "";
-            document.getElementById('name').value = "";
-            // Clear checkboxes
-            document.querySelectorAll('input[type=checkbox]').forEach(checkbox => checkbox.checked = false);
-            // Add other fields as needed
-        } else {
-            // Find the selected site data
-            var selectedSite = sitesData.find(site => site.id == selectedSiteId);
-            console.log('Selected Site:', selectedSite); // Debugging
-
-            if (selectedSite) {
-                // Populate form fields with selected site data, use empty string as default if undefined
-                document.getElementById('company').value = selectedSite.site_data.company || "";
-                document.getElementById('name').value = selectedSite.site_name || "";
-
-                // Clear checkboxes
-                document.querySelectorAll('input[type=checkbox]').forEach(checkbox => checkbox.checked = false);
-
-                //set type radio select
-                if (selectedSite.site_data.type == "1") {
-                    document.getElementById('type-1').checked = true;
-                }
-                if (selectedSite.site_data.type == "2") {
-                    document.getElementById('type-2').checked = true;
-                }
-                if (selectedSite.site_data.type == "3") {
-                    document.getElementById('type-3').checked = true;
-                }
-
-                //set org checkboxes
-                if (0 in selectedSite.site_data.org) {
-                    document.getElementById('org-0').checked = true;
-                }
-                if (selectedSite.site_data.org.indexOf("servitex") !== -1) {
-                    document.getElementById('org-servitex').checked = true;
-                }
-                if (selectedSite.site_data.org.indexOf("sitex") !== -1) {
-                    document.getElementById('org-sitex').checked = true;
-                }
-                if (selectedSite.site_data.org.indexOf("dressline") !== -1) {
-                    document.getElementById('org-dressline').checked = true;
-                }
-                if (selectedSite.site_data.org.indexOf("nwdzentratex") !== -1) {
-                    document.getElementById('org-nwdzentratex').checked = true;
-                }
-                if (selectedSite.site_data.org.indexOf("lavantex") !== -1) {
-                    document.getElementById('org-lavantex').checked = true;
-                }
-                if (selectedSite.site_data.org.indexOf("tsa") !== -1) {
-                    document.getElementById('org-tsa').checked = true;
-                }
-                if (selectedSite.site_data.org.indexOf("leosystem") !== -1) {
-                    document.getElementById('org-leosystem').checked = true;
-                }
-                if (selectedSite.site_data.org.indexOf("dbl") !== -1) {
-                    document.getElementById('org-dbl').checked = true;
-                }
-                if (selectedSite.site_data.org.indexOf("diemietwaesche.de") !== -1) {
-                    document.getElementById('org-diemietwaesche.de').checked = true;
-                }
-
-                // set work checkboxes
-                if (selectedSite.site_data.work.indexOf("wtabwasserfrisch") !== -1) {
-                    document.getElementById('work1-wtabwasserfrisch').checked = true;
-                    document.getElementById('work3-wtabwasserfrisch').checked = true;
-                }
-                if (selectedSite.site_data.work.indexOf("wrabluftzufrisch") !== -1) {
-                    document.getElementById('work1-wrabluftzufrisch').checked = true;
-                }
-                if (selectedSite.site_data.work.indexOf("wrabluftzuwasser") !== -1) {
-                    document.getElementById('work1-wrabluftzuwasser').checked = true;
-                    document.getElementById('work3-wrabluftzuwasser').checked = true;
-                }
-                if (selectedSite.site_data.work.indexOf("wasserrueck") !== -1) {
-                    document.getElementById('work1-wasserrueck').checked = true;
-                    document.getElementById('work2-wasserrueck').checked = true;
-                    document.getElementById('work3-wasserrueck').checked = true;
-                }
-                if (selectedSite.site_data.work.indexOf("wassermehrfach") !== -1) {
-                    document.getElementById('work1-wassermehrfach').checked = true;
-                }
-                if (selectedSite.site_data.work.indexOf("abwassersiebfilter") !== -1) {
-                    document.getElementById('work1-abwassersiebfilter').checked = true;
-                }
-                if (selectedSite.site_data.work.indexOf("abwasserfaellung") !== -1) {
-                    document.getElementById('work1-abwasserfaellung').checked = true;
-                }
-                if (selectedSite.site_data.work.indexOf("abwassermikrofiltration") !== -1) {
-                    document.getElementById('work1-abwassermikrofiltration').checked = true;
-                }
-                if (selectedSite.site_data.work.indexOf("abwasserneutralis") !== -1) {
-                    document.getElementById('work1-abwasserneutralis').checked = true;
-                }
-                if (selectedSite.site_data.work.indexOf("abwasserbiologie") !== -1) {
-                    document.getElementById('work1-abwasserbiologie').checked = true;
-                }
-                if (selectedSite.site_data.work.indexOf("dampfkessel") !== -1) {
-                    document.getElementById('work1-dampfkessel').checked = true;
-                }
-                if (selectedSite.site_data.work.indexOf("dampfsystem") !== -1) {
-                    document.getElementById('work1-dampfsystem').checked = true;
-                }
-                if (selectedSite.site_data.work.indexOf("kontakt") !== -1) {
-                    document.getElementById('work2-kontakt').checked = true;
-                    document.getElementById('work3-kontakt').checked = true;
-                }
-                if (selectedSite.site_data.work.indexOf("kuehl") !== -1) {
-                    document.getElementById('work2-kuehl').checked = true;
-                    document.getElementById('work3-kuehl').checked = true;
-                }
-                if (selectedSite.site_data.work.indexOf("wrabluftzufluft") !== -1) {
-                    document.getElementById('work3-wrabluftzufluft').checked = true;
-                }
-
-
-                }
-            }
-        }
-    
-
-    window.onload = function() {
-
-            // Extract site_id from the URL, if available
-            var urlParams = new URLSearchParams(window.location.search);
-            var siteIdFromUrl = urlParams.has('site_id') ? urlParams.get('site_id') : null;
-
-            // Populate the dropdown and select the appropriate site if siteIdFromUrl is provided
-            populateDropdown(siteIdFromUrl);
-
-            document.getElementById('siteSelect').addEventListener('change', function() {
-                updateFormFields();
-            });
-
-            document.getElementById('siteSelect').style.visibility = "hidden";
-
-        document.getElementById('deleteSite').addEventListener('click', function() {
-
-        event.preventDefault(); // dont transmit form
-
-        var select = document.getElementById('siteSelect');
-        var selectedSiteId = select.value;
-
-        if (selectedSiteId === "new") {
-            alert("Bitte wählen Sie eine vorhandene Seite aus, um sie zu löschen.");
-            return;
-        }
-
-        if (confirm("Sind Sie sicher, dass Sie diese Seite löschen möchten?")) {
-            var xhr = new XMLHttpRequest();
-            xhr.open("POST", "backend/delete_site.php", true);
-            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === 4 && xhr.status === 200) {
-                    alert(xhr.responseText);
-                    // Remove the deleted entry from the dropdown and select the previous one
-                    var optionToRemove = select.querySelector('option[value="' + selectedSiteId + '"]');
-                    if (optionToRemove) {
-                        var previousOption = optionToRemove.previousElementSibling;
-                        optionToRemove.remove();
-                        if (previousOption) {
-                            previousOption.selected = true;
-                        } else {
-                            select.value = "new";
-                        }
-                        updateFormFields();
-                    }
-                    // Redirect to dashboard.php after deletion
-                    window.location.href = 'dashboard.php';
-                }
-            };
-            xhr.send("id=" + selectedSiteId);
-        }
-        });
-
-        // If a site ID is provided in the URL, update the form fields
-        if (siteIdFromUrl !== null) {
-            updateFormFields(siteIdFromUrl);
-        }
-    }
-</script>
 
 <script src="datenerfassung.js"></script>
 
